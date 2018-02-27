b6d07d72dcc5a4111ed79690c947cf805e8e920f
duangduangbo <575620657@qq.com>
2018-02-27 23:31:47 +0800

    定位待完善

--- platforms/android/app/build/intermediates/assets/debug/www/js/controllers.js
+++ platforms/android/app/build/intermediates/assets/debug/www/js/controllers.js
@@ -12,29 +12,41 @@ $scope.onTrack=function(){
 
 var map = new BMap.Map("allmap");    // 创建Map实例
 
-map.centerAndZoom(new BMap.Point(116.404, 39.915), 11);  // 初始化地图,设置中心点坐标和地图级别
+map.centerAndZoom(new BMap.Point(11.404, 39.915), 11);  // 初始化地图,设置中心点坐标和地图级别
 map.addControl(new BMap.MapTypeControl());   //添加地图类型控件
-map.setCurrentCity("北京");          // 设置地图显示的城市 此项是必须设置的
-map.enableScrollWheelZoom(true);     //开启鼠标滚轮缩放
-console.log(map);
-document.getElementById("getPosition").addEventListener("click", getPosition); 　　//引用手机GPS定位
-function getPosition() {
-  var options = {
-    enableHighAccuracy: true,
-    maximumAge: 3600000
-  }
-   var watchID = navigator.geolocation.getCurrentPosition(onSuccess, onError, options);
-    function onSuccess(position) {
-      var lat = position.coords.latitude;
-      var long = position.coords.longitude
-      alert('Latitude: ' + position.coords.latitude + '\n'+
-        'Longitude: ' + position.coords.longitude +'\n');
-      map.centerAndZoom(new BMap.Point(lat, long), 7);
-    };
-    function onError(error) {
-      alert('无法获取地理位置');
-    }
-  }
+//map.setCurrentCity("北京");          // 设置地图显示的城市 此项是必须设置的
+//map.enableScrollWheelZoom(true);     //开启鼠标滚轮缩放
+//console.log(map);
+//document.getElementById("getPosition").addEventListener("click", getPosition); 　　//引用手机GPS定位
+//function getPosition() {
+//  var options = {
+//    enableHighAccuracy: true,
+//    maximumAge: 3600000
+//  }
+//   var watchID = navigator.geolocation.getCurrentPosition(onSuccess, onError, options);
+//    function onSuccess(position) {
+//      var lat = position.coords.latitude;
+//      var long = position.coords.longitude
+//      alert('Latitude: ' + position.coords.latitude + '\n'+
+//        'Longitude: ' + position.coords.longitude +'\n');
+//      map.centerAndZoom(new BMap.Point(lat, long), 7);
+//    };
+//    function onError(error) {
+//      alert('无法获取地理位置');
+//    }
+//  }
+var geolocation = new BMap.Geolocation();
+geolocation.getCurrentPosition(function(r){
+	if(this.getStatus() == BMAP_STATUS_SUCCESS){
+		var mk = new BMap.Marker(r.point);
+		map.addOverlay(mk);
+		map.panTo(r.point);
+		alert('您的位置：'+r.point.lng+','+r.point.lat);
+	}
+	else {
+		alert('failed'+this.getStatus());
+	}
+});
 }])
    
 .controller('page2Ctrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
--- platforms/browser/www/js/controllers.js
+++ platforms/browser/www/js/controllers.js
@@ -9,32 +9,61 @@ $scope.onTrack=function(){
     $state.go("page4");
     console.log('page1');
 }
+var map = new BMap.Map("allmap");
+var point = new BMap.Point(116.331398,39.897445);
+map.centerAndZoom(point,12);
 
-var map = new BMap.Map("allmap");    // 创建Map实例
-
-map.centerAndZoom(new BMap.Point(116.404, 39.915), 11);  // 初始化地图,设置中心点坐标和地图级别
-map.addControl(new BMap.MapTypeControl());   //添加地图类型控件
-map.setCurrentCity("北京");          // 设置地图显示的城市 此项是必须设置的
-map.enableScrollWheelZoom(true);     //开启鼠标滚轮缩放
-console.log(map);
-document.getElementById("getPosition").addEventListener("click", getPosition); 　　//引用手机GPS定位
-function getPosition() {
-  var options = {
-    enableHighAccuracy: true,
-    maximumAge: 3600000
-  }
-   var watchID = navigator.geolocation.getCurrentPosition(onSuccess, onError, options);
-    function onSuccess(position) {
-      var lat = position.coords.latitude;
-      var long = position.coords.longitude
-      alert('Latitude: ' + position.coords.latitude + '\n'+
-        'Longitude: ' + position.coords.longitude +'\n');
-      map.centerAndZoom(new BMap.Point(lat, long), 7);
-    };
-    function onError(error) {
-      alert('无法获取地理位置');
-    }
-  }
+//var map = new BMap.Map("allmap");    // 创建Map实例
+//
+//map.centerAndZoom(new BMap.Point(116.404, 39.915), 12);  // 初始化地图,设置中心点坐标和地图级别
+//map.addControl(new BMap.MapTypeControl());   //添加地图类型控件
+//map.setCurrentCity("北京");          // 设置地图显示的城市 此项是必须设置的
+//map.enableScrollWheelZoom(true);     //开启鼠标滚轮缩放
+//console.log(map);
+//document.getElementById("getPosition").addEventListener("click", getPosition); 　　//引用手机GPS定位
+//function getPosition() {
+//  var options = {
+//    enableHighAccuracy: true,
+//    maximumAge: 3600000
+//  }
+//   var watchID = navigator.geolocation.getCurrentPosition(onSuccess, onError, options);
+//    function onSuccess(position) {
+//      var lat = position.coords.latitude;
+//      var long = position.coords.longitude
+//      alert('Latitude: ' + position.coords.latitude + '\n'+
+//        'Longitude: ' + position.coords.longitude +'\n');
+//      map.centerAndZoom(new BMap.Point(lat, long), 7);
+//    };
+//    function onError(error) {
+//      alert('无法获取地理位置');
+//    }
+//  }
+//var geolocation = new BMap.Geolocation();
+//geolocation.getCurrentPosition(function(r){
+//	if(this.getStatus() == BMAP_STATUS_SUCCESS){
+//		var mk = new BMap.Marker(r.point);
+//		map.addOverlay(mk);
+//		map.panTo(r.point);
+//		alert('您的位置：'+r.point.lng+','+r.point.lat);
+//	}
+//	else {
+//		alert('failed'+this.getStatus());
+//	}
+//});
+var geolocation = new BMap.Geolocation();
+// 开启SDK辅助定位
+geolocation.enableSDKLocation();
+geolocation.getCurrentPosition(function(r){
+	if(this.getStatus() == BMAP_STATUS_SUCCESS){
+		var mk = new BMap.Marker(r.point);
+		map.addOverlay(mk);
+		map.panTo(r.point);
+		alert('您的位置：'+r.point.lng+','+r.point.lat);
+	}
+	else {
+		alert('failed'+this.getStatus());
+	}
+});
 }])
    
 .controller('page2Ctrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
