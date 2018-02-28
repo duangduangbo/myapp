0f5d565a1cdbc453f1627684814ad59096fd7579
duangduangbo <575620657@qq.com>
2018-02-28 12:56:47 +0800

    继续完善定位问题

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -10,24 +10,50 @@ $scope.onTrack=function(){
     console.log('page1');
 }
 var map = new BMap.Map("allmap");
-var point = new BMap.Point(116.331398,39.897445);
-map.centerAndZoom(point,12);
+
+initMap();
 
 
+function initMap(){
+	var point = new BMap.Point(116.331398,39.897445);
+	map.centerAndZoom(point,11);//初始化地图
+	map.enableScrollWheelZoom(true);     //开启鼠标滚轮缩放
+	//添加地图类型控件
+	map.addControl(new BMap.MapTypeControl({
+		mapTypes:[
+			BMAP_NORMAL_MAP,
+			BMAP_HYBRID_MAP
+		]}));	//地图类型
+		map.addControl(new BMap.NavigationControl());//平移缩放控件
+		map.addControl(new BMap.ScaleControl());    //比例尺
+		map.addControl(new BMap.OverviewMapControl()); //缩略地图
+		map.addControl(new BMap.GeolocationControl()); //定位
+		
+
+//sdk定位
 var geolocation = new BMap.Geolocation();
-// 开启SDK辅助定位
-geolocation.enableSDKLocation();
+geolocation.enableSDKLocation();// 开启SDK辅助定位
 geolocation.getCurrentPosition(function(r){
+	console.log(r.coords);
 	if(this.getStatus() == BMAP_STATUS_SUCCESS){
 		var mk = new BMap.Marker(r.point);
-		map.addOverlay(mk);
-		map.panTo(r.point);
-		alert('您的位置：'+r.point.lng+','+r.point.lat);
+		map.addOverlay(mk);//设置覆盖物
+		map.panTo(r.point);//设置这个点为地图中心
+		// alert('您的位置：'+r.point.lng+','+r.point.lat);
+		
+		var point = new BMap.Point(r.point.lng,r.point.lat);//用所定位的经纬度查找所在地省市街道等信息
+		var gc = new BMap.Geocoder();
+		gc.getLocation(point, function(rs){
+		   var addComp = rs.addressComponents;
+		    console.log(rs.address);//地址信息
+		   alert(rs.address);//弹出所在地址
+		});
 	}
 	else {
 		alert('failed'+this.getStatus());
-	}
-});
+	}        
+},{enableHighAccuracy: true});
+}
 }])
    
 .controller('page2Ctrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
